3b5e3330979041fe310ce7fddc8eba4549114942
ghtlspakghtl <b01095462649x@gmail.com>
2026-08-23 02:25:56 +0000

    fix: update files

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ export default function App() {
   const GOOGLE_FORM_URL = "https://forms.gle/joL7ZUq6V7AAoSkd7";
 
   return (
-    <div className="container">
+    <div className="bg-container">
+      <div className="overlay"></div>
       <div className="card">
         <h1>수학체험부 방탈출 행사</h1>
         <p className="description">
           아래 버튼을 눌러 참가 신청서를 작성해 주세요.
         </p>
-
         <a 
           href={GOOGLE_FORM_URL} 
           target="_blank" 
